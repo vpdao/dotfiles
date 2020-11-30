a251df5b5380213349fdac26316d04836643f764
vpdao <phuong.dao@ngs-sensors.it>
2020-11-30 17:43:43 +0100

    add configuraiton for shell

---
+++ shell/aliases.sh
@@ -0,0 +1,23 @@
+# Use colors in coreutils utilities output
+alias ls='ls --color=auto'
+alias grep='grep --color'
+
+# ls aliases
+alias ll='ls -lah'
+alias la='ls -A'
+alias l='ls'
+
+# Aliases to protect overwritting
+alias cp='cp -i'
+alias mv='mv -i'
+
+# git related aliases
+alias gs='git status'
+alias gc='git commit'
+alias gp='git push'
+alias gb='git branch'
+alias gbr='git branch -r'
+alias gd='git diff'
+alias ga='git add'
+alias gr='git rebase'
+
