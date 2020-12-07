3cbf60d612754cb428fb062ea59f8bd9750f9c37
vpdao <phuong.dao@ngs-sensors.it>
2020-12-07 10:13:13 +0100

    add valgrind.zsh configuration for valgrind and callgrind

---
+++ zsh/valgrind.zsh
@@ -0,0 +1,10 @@
+# export installation path for valgrind and callgrind
+export PATH=$PATH:/opt/valgrind/bin
+
+export PATH=$PATH:/opt/callgrind_annotate
+
+export PATH=$PATH:/opt/callgrind_control
+
+export VALGRIND_LIB="/opt/valgrind/lib/valgrind"
+
+
